c0c0cb485090522fceda62554b3fe99e65990039
Noamaan Mulla <noamaan.mulla03@gmail.com>
2026-04-11 13:45:50 +0530

    feat: same as before

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -5,29 +5,37 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
+    '/lib/**/*', // Ignore built files.
+    '/generated/**/*', // Ignore generated files.
   ],
   plugins: [
-    "@typescript-eslint",
-    "import",
+    '@typescript-eslint',
+    'import',
   ],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    'quotes': ['error', 'single'],
+    'import/no-unresolved': 0,
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'max-len': ['error', { 'code': 120 }],
+    'object-curly-spacing': ['error', 'always'],
+    'require-jsdoc': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'valid-jsdoc': 0,
+    'operator-linebreak': ['error', 'after'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
   },
 };
